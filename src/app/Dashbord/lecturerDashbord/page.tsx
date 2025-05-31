e075c81eee140c4ade3bf9988f65a4572dcce1ab
Yun4g <delightvincent487@gmail.com>
2025-05-31 15:33:01 +0100

    push new update type

--- src/app/Dashbord/lecturerDashbord/page.tsx
+++ src/app/Dashbord/lecturerDashbord/page.tsx
@@ -1,30 +1,27 @@
 "use client";
-import useComponentStore from "@/app/store/ComponentIDStore"
+
+export const dynamic = 'force-dynamic';
+
+import useComponentStore from "@/app/store/ComponentIDStore";
 import LecturerForms from "@/componentes/LectureForms";
 import ManageResults from "@/componentes/manageResults";
 
-
-
-
-
-
-
 export default function LecturerDashBoard() {
-    const ComponentPage = useComponentStore((state)=> state.ComponentPage)
-    
-    return(
-        <div className=" px-3 h-full   pt-4 w-full">
-             {
-                 ComponentPage === "Form" ? (
-                      <div>
-                          <LecturerForms/>
-                      </div>
-                 ) : (
-                       <section>
-                           <ManageResults/>
-                       </section>
-                 )
-             }
+    const ComponentPage = useComponentStore((state)=> state.ComponentPage);
+
+    return (
+        <div className="px-3 h-full pt-4 w-full">
+            {
+                ComponentPage === "Form" ? (
+                    <div>
+                        <LecturerForms />
+                    </div>
+                ) : (
+                    <section>
+                        <ManageResults />
+                    </section>
+                )
+            }
         </div>
-    )
+    );
 }
