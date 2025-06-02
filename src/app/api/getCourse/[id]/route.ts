225e83b22443ff39ab93f10b7f1c9c7ac1045503
Yun4g <delightvincent487@gmail.com>
2025-06-02 08:50:24 +0100

    quiz project

--- src/app/api/getCourse/[id]/route.ts
+++ src/app/api/getCourse/[id]/route.ts
@@ -1,26 +1,32 @@
 import connectDB from "@/lib/mongoose";
 import { NextRequest, NextResponse } from "next/server";
 
+export async function GET(
+  request: NextRequest,
+  { params }: { params: Record<string, string> }
+) {
+  const { id } = params;
 
+  try {
+    await connectDB();
+    const CourseModel = (await import("@/models/questionSchema")).default;
+    const course = await CourseModel.findById(id);
 
-
-
-
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-    const {id} = params;
-
-    try {
-        await connectDB();
-        const CourseModel = (await import("@/models/questionSchema")).default;
-        const course = await CourseModel.findById(id);
-        if (!course) {
-            return new NextResponse(JSON.stringify({ message: "Course not found" }), { status: 404 });
-        }
-
-        return new NextResponse(JSON.stringify(course), { status: 200, headers: { "Content-Type": "application/json" } });
-        
-    } catch (error) {
-        return new NextResponse(JSON.stringify({ message: "Internal Server Error" }), { status: 500 });
-        console.error("Error fetching course:", error);
+    if (!course) {
+      return new NextResponse(JSON.stringify({ message: "Course not found" }), {
+        status: 404,
+      });
     }
+
+    return new NextResponse(JSON.stringify(course), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    console.error("Error fetching course:", error);
+    return new NextResponse(
+      JSON.stringify({ message: "Internal Server Error" }),
+      { status: 500 }
+    );
+  }
 }
